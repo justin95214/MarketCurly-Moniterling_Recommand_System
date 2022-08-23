5689870fcb6f9a3a782380c0b696d04562815628
ParkJinYong <mej1406@gmail.com>
2022-08-23 21:05:51 +0900

    0823-1948 requirements update

--- util/slack.py
+++ util/slack.py
@@ -1,12 +1,14 @@
 import requests
 
+
 def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
-        data={"channel": channel,"text": text}
-    )
+                             headers={"Authorization": "Bearer "+token},
+                             data={"channel": channel, "text": text}
+                             )
     print(response)
+
 
 myToken = "xoxb-3963479472818-3957191170422-zzsqS0r3UecaJT6MbthvTz6q"
 
-post_message(myToken,"#공모전","test")
+post_message(myToken, "#공모전", "test")
