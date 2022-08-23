89098f1c77f267746285bec45a0e0e3a4ca1a1d7
ParkJinYong <mej1406@gmail.com>
2022-08-23 22:21:14 +0900

    0823-2216 gmarket weight modify

--- crawling_file/source/crawling.py
+++ crawling_file/source/crawling.py
@@ -93,6 +93,7 @@ class EmartCrawling(Crawling):
                         location_list = []
                         candid_name = list(name.split(" "))
 
+                        weight = ''
                         for m in candid_name:
                             if 'kg' in m or 'KG' in m:
                                 try:
