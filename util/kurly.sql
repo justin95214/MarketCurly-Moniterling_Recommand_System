99f0a5dd8c06a77f43567879758c11d91626d695
ParkJinYong <mej1406@gmail.com>
2022-08-23 19:48:09 +0900

    0823-1948 kurly_sql update

--- util/kurly.sql
+++ util/kurly.sql
@@ -1,48 +1,4 @@
--- create table kurly.naver (
---     date datetime,
---     title varchar(500),
---     price int,
---     weight varchar(500),
---     kind varchar(500),
---     site varchar(500),
---     location varchar(500)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- alter table kurly.naver add primary key(date, title, price);
-
--- create table kurly.gmarket (
---     date datetime,
---     title varchar(500),
---     price int,
---     weight varchar(500),
---     kind varchar(500),
---     site varchar(500),
---     location varchar(500)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- alter table kurly.gmarket add primary key(date, title, price);
-
--- create table kurly.coupang (
---     date datetime,
---     title varchar(500),
---     price int,
---     weight varchar(500),
---     kind varchar(500),
---     site varchar(500),
---     location varchar(500)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- alter table kurly.coupang add primary key(date, title, price);
-
--- create table kurly.emart (
---     date datetime,
---     title varchar(500),
---     price int,
---     weight varchar(500),
---     kind varchar(500),
---     site varchar(500),
---     location varchar(500)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- alter table kurly.emart add primary key(date, title, price);
-
-create table kurly.Total (
+create table daduckDB.naver (
     date datetime,
     title varchar(500),
     price int,
@@ -51,6 +7,50 @@ create table kurly.Total (
     site varchar(500),
     location varchar(500)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-alter table kurly.Total add primary key(date, title, price);
+alter table daduckDB.naver add primary key(date, title, price);
 
-/*select * FROM kurly.naver Limit 2000;*/
+create table daduckDB.gmarket (
+    date datetime,
+    title varchar(500),
+    price int,
+    weight varchar(500),
+    kind varchar(500),
+    site varchar(500),
+    location varchar(500)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+alter table daduckDB.gmarket add primary key(date, title, price);
+
+create table daduckDB.coupang (
+    date datetime,
+    title varchar(500),
+    price int,
+    weight varchar(500),
+    kind varchar(500),
+    site varchar(500),
+    location varchar(500)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+alter table daduckDB.coupang add primary key(date, title, price);
+
+create table daduckDB.emart (
+    date datetime,
+    title varchar(500),
+    price int,
+    weight varchar(500),
+    kind varchar(500),
+    site varchar(500),
+    location varchar(500)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+alter table daduckDB.emart add primary key(date, title, price);
+
+create table daduckDB.Total (
+    date datetime,
+    title varchar(500),
+    price int,
+    weight varchar(500),
+    kind varchar(500),
+    site varchar(500),
+    location varchar(500)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+alter table daduckDB.Total add primary key(date, title, price);
+
+/*select * FROM daduckDB.naver Limit 2000;*/
