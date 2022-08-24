c547946cdd2956bbb32742fce7ba8e678dbdffd0
ParkJinYong <mej1406@gmail.com>
2022-08-24 09:43:15 +0900

    0824-0942

--- web_file/final/polls/views.py
+++ web_file/final/polls/views.py
@@ -119,6 +119,7 @@ def submit(request):
     city_list = ['서울특별시','부산광역시','대구광역시','인천광역시','광주광역시','대전광역시','울산광역시','세종특별자치시','경기도','강원도','충청북도','충청남도','전라북도','전라남도','경상북도','경상남도','제주특별자치도']
     temp_list = []
     for market in market_list:
+        print(data['location'])
         temp = data.loc[data['location'] == market]
         print(temp)
         print(temp.shape)
